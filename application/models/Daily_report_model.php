669eae07a5cc4e9c6458d6617f800b0e8d44385d
照井拓朗 <>
2021-07-16 15:47:48 +0900

    gizlog日報編集画面実装

--- application/models/Daily_report_model.php
+++ application/models/Daily_report_model.php
@@ -7,7 +7,15 @@ class Daily_report_model extends CI_Model
         $this->load->database();
     }
 
-    public function create_report()
+    protected $dates = [
+      'reporting_time'
+    ];
+
+    protected $casts = [
+      'reporting_time' => 'datetime',
+    ];
+
+    public function createReport()
     {
       $today = date("Y-m-d H:i:s");
 
@@ -22,4 +30,24 @@ class Daily_report_model extends CI_Model
 
       return $this->db->insert('daily_reports', $data);
       }
+
+      public function getDailyReport($id)
+      {
+          $query = $this->db->get_where('daily_reports', array('id' => $id));
+          return $query->row_array();
+      }
+
+      public function editReport($id)
+      {
+        $today = date("Y-m-d H:i:s");
+
+        $data = array(
+            'title' => $this->input->post('title'),
+            'content' => $this->input->post('content'),
+            'reporting_time' => $this->input->post('date'),
+            'update_at' => $today
+        );
+
+        return $this->db->where('id', $id)->update('daily_reports', $data);
+        }
 }
--- application/views/user/daily_report/create_daily_report.php
+++ application/views/user/daily_report/create_daily_report.php
@@ -5,21 +5,18 @@
       <div class="form-group form-size-small <?php if (form_error('date')): ?>
         has-error
       <?php endif; ?>">
-      <!-- <div class="form-group form-size-small has-error"> -->
         <?php echo form_input(array('name' => 'date', 'type' => 'date', 'class' => 'form-control')) ?>
         <span class="help-block"><?php echo form_error('date'); ?></span>
       </div>
       <div class="form-group <?php if (form_error('date')): ?>
         has-error
       <?php endif; ?>">
-      <!-- <div class="form-group has-error"> -->
         <?php echo form_input(array('name' => 'title', 'type' => 'text', 'class' => 'form-control', 'placeholder' => 'Title')) ?>
         <span class="help-block"><?php echo form_error('title'); ?></span>
       </div>
       <div class="form-group <?php if (form_error('date')): ?>
         has-error
       <?php endif; ?>">
-      <!-- <div class="form-group has-error"> -->
         <?php echo form_textarea(array('name' => 'content', 'class' => 'form-control', 'placeholder' => 'Content', 'cols' => 50, 'rows' => 10)) ?>
         <span class="help-block"><?php echo form_error('content'); ?></span>
       </div>
