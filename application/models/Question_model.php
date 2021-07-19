aa21be0af0bf3a528902cbc7086023e221ff6af9
照井拓朗 <>
2021-07-19 11:46:17 +0900

    Gizlog質問マイページ実装

--- application/models/Question_model.php
+++ application/models/Question_model.php
@@ -7,7 +7,7 @@ class Question_model extends CI_Model
         $this->load->database();
     }
 
-    public function serchQuestions()
+    public function serchQuestions($perPage, $pageNum)
     {
         $serchWord = $this->input->get('search_word');
         $tagCategoryId = $this->input->get('tag_category_id');
@@ -23,6 +23,7 @@ class Question_model extends CI_Model
         if (isset($tagCategoryId) && $tagCategoryId!== '') {
           $this->db->where('tag_category_id', $tagCategoryId);
         }
+        $this->db->limit($perPage, $perPage * $pageNum);
         $query = $this->db->get();
         return $query->result_array();
       }
@@ -31,5 +32,32 @@ class Question_model extends CI_Model
     {
         $query = $this->db->get('questions');
         return count($query->result_array());
+    }
+
+    public function getByUserId($perPage, $pageNum, $userId)
+    {
+        $this->db->select('*, tag_categories.name AS category_name');
+        $this->db->from('questions');
+        $this->db->where('questions.deleted_at', null);
+        $this->db->where('questions.user_id', $userId);
+        $this->db->order_by('questions.created_at', 'desc');
+        $this->db->join('tag_categories', 'tag_categories.id = questions.tag_category_id', 'left');
+        $this->db->join('users', 'users.id = questions.user_id', 'left');
+        $this->db->join('(SELECT question_id, COUNT(*) AS cnt FROM comments GROUP BY question_id) as comments', 'comments.question_id = questions.id', 'left outer');
+        $this->db->limit($perPage, $perPage * $pageNum);
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function deleteReport($id)
+    {
+      $today = date("Y-m-d H:i:s");
+
+      $data = array(
+          'update_at' => $today,
+          'deleted_at' => $today
+      );
+
+      return $this->db->where('id', $id)->update('questions', $data);
     }
 }
